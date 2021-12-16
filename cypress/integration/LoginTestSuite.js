73f27284b5af665cee419a399c242fe2db8c42ad
hstravinskas <haroldas.stravinskas97@gmail.com>
2021-12-16 14:58:17 +0200

    Few fixes to the Login test suite

--- cypress/integration/LoginTestSuite.js
+++ cypress/integration/LoginTestSuite.js
@@ -1,14 +1,17 @@
 /// <reference types="Cypress"/>
 const HomepagePO = require('../support/pageObjects/the-internet/the-internet_homepage_po');
-const LoginPO = require('../support/pageObjects/the-internet/the-internet-login-po')
+const LoginPO = require('../support/pageObjects/the-internet/the-internet-login-po');
 const hPO = new HomepagePO;
 const lPO = new LoginPO;
 
 describe('Test the login functionality', () => {
+    
     it('Verify that user is able to login', () => {
         cy.visit('/');
         hPO.navigateToLogin();
-        lPO.performLogin('tomsmith','SuperSecretPassword!')
+        cy.fixture("../fixtures/login_data.json").then((user) => {
+            lPO.performLogin(user.correct.username,user.correct.password)
+        })
         cy.get('#flash').should('contain.text','You logged into a secure area')
         cy.get('[href="/logout"]').should('exist')
     });
@@ -16,14 +19,18 @@ describe('Test the login functionality', () => {
     it('Verify that user gets an error when entering wrong username', () => {
         cy.visit('/');
         hPO.navigateToLogin();
-        lPO.performLogin('invalid','SuperSecretPassword!')
+        cy.fixture("../fixtures/login_data.json").then((user) => {
+            lPO.performLogin(user.incorrectUsername.username,user.incorrectUsername.password)
+        })
         cy.get('#flash').should('contain.text', " Your username is invalid!")
     });
 
     it('Verify that user gets an error when entering wrong password', () => {
         cy.visit('/');
         hPO.navigateToLogin();
-        lPO.performLogin('tomsmith','asdasdada')
+        cy.fixture("../fixtures/login_data.json").then((user) => {
+            lPO.performLogin(user.incorrectPassword.username,user.incorrectPassword.password)
+        })
         cy.get('#flash').should('contain.text', "Your password is invalid!")
     });
 });
--- cypress/support/pageObjects/the-internet/the-internet-login-po.js
+++ cypress/support/pageObjects/the-internet/the-internet-login-po.js
@@ -3,7 +3,7 @@ class TheInternetLoginPO{
     performLogin(username, password){
         cy.get('#username').type(username);
         cy.get('#password').type(password);
-        cy.get('i.fa-sign-in').click({force:true})
+        cy.get('i.fa-sign-in').click({force:true});
     }
 };
 export default TheInternetLoginPO;
